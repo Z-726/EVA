a9ee4cea8ff76f49f30dd0466caca2655b616a69
lizhuo-1994 <guitar_318576604@yahoo.co.jp>
2022-11-17 23:42:05 +0900

    update

---
+++ scripts/Alien.sh
@@ -0,0 +1,5 @@
+python experiment.py --envname AlienNoFrameskip-v4
+python experiment.py --envname AlienNoFrameskip-v4
+python experiment.py --envname AlienNoFrameskip-v4
+python experiment.py --envname AlienNoFrameskip-v4
+python experiment.py --envname AlienNoFrameskip-v4
